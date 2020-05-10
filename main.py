8a10463460bc69d6e87dd41b8639b5cdb9ebd887
Sunny Choi <yukhengchoi@gmail.com>
2020-05-10 15:50:24 +0800

    add os path join function(auto)

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import os
 
 for file in os.listdir('orig'): # . 現在位置
 	if file.endswith('.jpg'):
-		image_file = Image.open('orig/' + file) # open colour image
+		image_file = Image.open(os.path.join('orig', file)) # open colour image
 		image_file = image_file.convert('L') # convert image to black and white
-		image_file.save('result/' + file[:-4] + '_bw.png')
+		image_file.save(os.path.join('result', file[:-4] + '_bw.png'))
